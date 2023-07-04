2521322613137b977ff2fb9b760a6906e9d39494
Robin Burri <robin@Robins-MacBook-Air.local>
2023-07-04 21:53:38 +0200

    boiler done

---
+++ client/script.js
@@ -0,0 +1,26 @@
+const joinRoomButton = document.getElementById("room-button");
+const messageInuput = document.getElementById("message-input");
+const roomInput = document.getElementById("room-input");
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInuput.value;
+  const room = roomInput.value;
+
+  if (message.trim() === "") return;
+  displayMessage(message);
+  messageInuput.value = "";
+});
+
+joinRommoButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const room = roomInput.value;
+})
+
+const displayMessage = (message) => {
+    const div = document.createElement("div");
+    div.textContent = message;
+    document.getElementById("message-container").appendChild(div);
+    
+}
