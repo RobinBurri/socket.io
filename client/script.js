35517a1df24166158a2dbf934cfc12f7d87c3cfc
Robin Burri <robin@Robins-MacBook-Air.local>
2023-07-04 22:21:10 +0200

    must add snowpack

--- client/script.js
+++ client/script.js
@@ -1,7 +1,11 @@
+import { io } from "socket.io-client";
+
 const joinRoomButton = document.getElementById("room-button");
 const messageInuput = document.getElementById("message-input");
 const roomInput = document.getElementById("room-input");
 const form = document.getElementById("form");
+
+const socket = io("http://localhost:3000");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -16,11 +20,10 @@ form.addEventListener("submit", (e) => {
 joinRommoButton.addEventListener("click", (e) => {
   e.preventDefault();
   const room = roomInput.value;
-})
+});
 
 const displayMessage = (message) => {
-    const div = document.createElement("div");
-    div.textContent = message;
-    document.getElementById("message-container").appendChild(div);
-    
-}
+  const div = document.createElement("div");
+  div.textContent = message;
+  document.getElementById("message-container").appendChild(div);
+};
---
+++ server/index.js
@@ -0,0 +1,7 @@
+const io = require("socket.io")(3000, {
+  cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+});
