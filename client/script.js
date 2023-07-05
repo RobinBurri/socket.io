264be071ef9a7a32a1dd8c83e0111c7dabac26ca
Robin Burri <rburri@c1r16s5.42lausanne.ch>
2023-07-05 09:20:53 +0200

    formated

--- client/script.js
+++ client/script.js
@@ -4,22 +4,22 @@ const roomInput = document.getElementById("room-input");
 const form = document.getElementById("form");
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const message = messageInuput.value;
-  const room = roomInput.value;
+    e.preventDefault();
+    const message = messageInuput.value;
+    const room = roomInput.value;
 
-  if (message.trim() === "") return;
-  displayMessage(message);
-  messageInuput.value = "";
+    if (message.trim() === "") return;
+    displayMessage(message);
+    messageInuput.value = "";
 });
 
 joinRoomButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  const room = roomInput.value;
+    e.preventDefault();
+    const room = roomInput.value;
 });
 
 const displayMessage = (message) => {
-  const div = document.createElement("div");
-  div.textContent = message;
-  document.getElementById("message-container").appendChild(div);
+    const div = document.createElement("div");
+    div.textContent = message;
+    document.getElementById("message-container").appendChild(div);
 };
