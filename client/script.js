542d8b0059d548e8573f34988d148d67113d2717
Robin Burri <rburri@c1r16s5.42lausanne.ch>
2023-07-05 09:33:37 +0200

    CORS fixed

--- client/script.js
+++ client/script.js
@@ -5,25 +5,25 @@ const messageInuput = document.getElementById("message-input");
 const roomInput = document.getElementById("room-input");
 const form = document.getElementById("form");
 
-const socket = io('http://localhost:3000')
+const socket = io("http://localhost:3000");
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const message = messageInuput.value;
-  const room = roomInput.value;
+    e.preventDefault();
+    const message = messageInuput.value;
+    const room = roomInput.value;
 
-  if (message.trim() === "") return;
-  displayMessage(message);
-  messageInuput.value = "";
+    if (message.trim() === "") return;
+    displayMessage(message);
+    messageInuput.value = "";
 });
 
 joinRoomButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  const room = roomInput.value;
+    e.preventDefault();
+    const room = roomInput.value;
 });
 
 const displayMessage = (message) => {
-  const div = document.createElement("div");
-  div.textContent = message;
-  document.getElementById("message-container").appendChild(div);
+    const div = document.createElement("div");
+    div.textContent = message;
+    document.getElementById("message-container").appendChild(div);
 };
